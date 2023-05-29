ccc5e06ef823e23b011cfdb29e91b70a9142e2a3
Sean Dowdall <seanied1234567@gmail.com>
2023-05-29 15:15:15 +0200

    encoding the first 1000 characters of the text

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import requests
+
 #
 # url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
 # output_file = "input.txt"
@@ -20,3 +21,23 @@ chars = sorted(list(set(text)))
 vocab_size = len(chars)
 print(''.join(chars))
 print(vocab_size)
+# this above is our vocabulary
+
+# next we would like to create strategy to tokenize the input text, converting text to sequence of integers
+# as this is a character based language model we will be simply turning characters into integers
+# create mapping from characters to integers
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]  # encoder: take string and output list of integers
+decode = lambda l: ''.join([itos[i] for i in l])  # decoder: take a list of ints and outputs a string
+
+print(encode("Hii There"))
+print(decode(encode("Hii There")))
+
+# now let's encode the entire text dataset and store it into a torch.tensor
+
+import torch  # we use pytorch
+
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000]) # the 1000 characters we looked at earlier will to the gpt look like this
